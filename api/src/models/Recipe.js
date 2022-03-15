134c3dd872a01d4ac23b91f2b679abaa51a46e9c
javiirigoyen <javier_irigoyen96@hotmail.com>
2022-03-15 20:30:19 -0300

    work

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    dishSummary: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    punctuation: {
+      type: DataTypes.INTEGER,
+      
+    },
+    healthyFoodLevel: {
+      type: DataTypes.INTEGER,
+      
+    },
+    stepByStep: {
+      type: DataTypes.TEXT,
+      
+    },
+    image: {
+      type: DataTypes.STRING,
+      
+    },
+    createdInDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true,
     },
   });
 };
