0156b8d97cb335d6ff515947a4ee28d75189a43a
javiirigoyen <javier_irigoyen96@hotmail.com>
2022-03-17 11:20:44 -0300

    work

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const axios = require("axios")
+//
 
 
 const router = Router();
