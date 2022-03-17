362b3e0ae644fff240e01b0b2fc856547678c564
javiirigoyen <javier_irigoyen96@hotmail.com>
2022-03-17 19:27:40 -0300

    work

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,7 +2,8 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const axios = require("axios");
-const Recipe = require('../models/Recipe');
+//const Recipe = require('../models/Recipe');
+const {Diets, Recipe} = require("../db")
 
 
 
@@ -12,20 +13,21 @@ const router = Router();
 // Ejemplo: router.use('/auth', authRouter);
 
 const getApiInfo = async () => {
-    const urlApi = await axios.get("https://api.spoonacular.com/recipes/complexSearch?apiKey=91bb058f06c64a4a99fd166c25420e7a&addRecipeInformation=true")
-    const apiInfo = await urlApi.data.map(el => {
+    const urlApi = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=91bb058f06c64a4a99fd166c25420e7a&addRecipeInformation=true`)
+    const apiInfo = await urlApi.results.map(e => {
+       
         return {
-            name: el.name,
-            id : el.id,
-            summary: el.summary,
-            image: el.image,
-            healthScore: el.healthScore,
-            spoonacularScore: el.spoonacularScore,
-            steps: el.steps
+            name: e.title,
+            id : e.id,
+            summary: e.summary,
+            image: e.image,
+            healthScore: e.healthScore,
+            spoonacularScore: e.spoonacularScore,
+            steps: e.steps.map(e => e)
 
         }
     })
-
+      
     return apiInfo
 }
 
@@ -42,11 +44,23 @@ const getDbInfo = async () => {
 }
 
 const getAllRecipes = async () => {
-    const apiInfo = await getApiInfo()
-    const dbInfo = await getDbInfo()
-    const allInfo = apiInfo.concat(dbInfo)
+    const apiInfos = await getApiInfo()
+    const dbInfos = await getDbInfo()
+    const allInfo = apiInfos.concat(dbInfos)
     return allInfo
 }
 
+router.get("/recipes", async (req, res) => {
+    const name = req.query.name
+    let recipesAll = await getAllRecipes()
+    if(name) {
+        let recipesName = await recipesAll.filter(e => e.title.toLowerCase().includes(name.toLowerCase))
+        recipesName.length ? 
+        res.status(200).send(recipesName) : 
+        res.status(404).send("no recipes found")
+    } else {
+        res.status(200).send(recipesAll)
+    }
+})
 
 module.exports = router;
